f3703b33dc718355d0a7c54a62ba3c76d2d8f8c8
rustamg070619 <rustamg070619@gmail.com>
2020-03-18 20:07:49 +0300

    commit2

--- src/main/java/servlet/AuthServlet.java
+++ src/main/java/servlet/AuthServlet.java
@@ -20,25 +20,4 @@ public class AuthServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getServletContext().getRequestDispatcher("/jsp/auth.jsp").forward(request, response);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String firstName = request.getParameter("firstName");
-        String password = request.getParameter("password");
-
-        if (userService.isExistUser(new User(firstName, password))) {
-            User user = userService.getUserByName(firstName);
-            String role = user.getRole();
-            if (role.equals("admin")) {
-                request.getServletContext().getRequestDispatcher("/home").forward(request, response);
-            } else if (role.equals("user")) {
-                request.getServletContext().getRequestDispatcher("/update").forward(request, response);
-            }
-        } else {
-            response.setCharacterEncoding("UTF-8");
-            PrintWriter out = response.getWriter();
-            out.println("Your login or password is incorrect!");
-        }
-
-    }
 }
